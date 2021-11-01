1b89f2e0433f7bb153f276dd74054d6d1b130ac1
Kevin Chan <kevinykchan@gmail.com>
2021-11-01 16:31:44 -0400

    Done with Add Event page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,28 @@
-export default function EventsPage() {
-    return (
-        <div>
-            <h1>My Events</h1>
-        </div>
-    )
+import Layout from '@/components/Layout'
+// import EventItem from '@/components/EventItem'
+import EventItem from '@/components/EvenItem'
+import {API_URL} from '@/components/config/index'
+
+export default function EventsPage({events}) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt}/>
+      ))}
+    </Layout>
+  )
+}
+
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
 }
